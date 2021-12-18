a743b2a9ea28c765f04f36ffc5c21608a2e3c44c
lordrizkyfaisal <96189968+lordrizkyfaisal@users.noreply.github.com>
2021-12-18 18:56:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -185,7 +185,7 @@ let antihidetag = setting.antihidetag
 banChats = setting.self_mode
 autorespon = true
 offline = false
-readGc = true 
+readGc = false 
 readPc = false 
 antitrol = false 
 bugc = false
@@ -194,9 +194,9 @@ autoketik = false
 autoregister = setting.user_register
 
 img = setting.img
-baper = 'SUB YT ZEEONE OFC'
-apiku = 'https://youtu.be/2rWbjrDIAyA'
-gc_wa_lu = 'https://chat.whatsapp.com/EU890BcXjyBDkNaUT5WmYV' //klo gk punya gc wa gk usah di ganti 👍
+baper = 'follow IG Rizky Faisal'
+apiku = 'https://www.instagram.com/p/CQcbqQ4hoF3/?utm_medium=copy_link'
+gc_wa_lu = 'https://chat.whatsapp.com/IlDt5VtbxKUAKWb86KEFFS' //klo gk punya gc wa gk usah di ganti 👍
 targetpc = setting.ownerNumberr
 owner = targetpc
 fake = setting.fake
