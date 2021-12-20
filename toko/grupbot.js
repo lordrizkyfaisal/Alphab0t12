5565654b5c96d02adef799c392ae1770b8ee9437
lordrizkyfaisal <96189968+lordrizkyfaisal@users.noreply.github.com>
2021-12-20 16:23:27 +0700

    Update and rename shop/grupbot.js to toko/grupbot.js

---
+++ toko/grupbot.js
@@ -0,0 +1,12 @@
+// jangan di ubah jika tidak punya group bot, takutnya eror
+const gcbotwa = () =>{
+	return`Join Aja Semua Fitur Bot Bisa Digunakan !
+
+1. *Alphabot Support*
+_https://chat.whatsapp.com/IlDt5VtbxKUAKWb86KEFFS_
+
+Jika ada link yang ke reset, silahkan hubungi
+owner untuk meminta link yang baru
+`
+}
+exports.gcbotwa = gcbotwa
