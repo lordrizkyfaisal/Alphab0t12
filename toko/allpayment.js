542c361b635b7294e60319752c75070fab86dbdc
lordrizkyfaisal <96189968+lordrizkyfaisal@users.noreply.github.com>
2021-12-20 14:37:53 +0700

    Update and rename shop/allpayment.js to toko/allpayment.js

---
+++ toko/allpayment.js
@@ -0,0 +1,20 @@
+const allpayment = (prefix) => {
+return `PAYMENT BY RIZKY
+	
+*💰 E-MONEY*
+
+1. GOPAY
+ㅁ 0882 1337 1423
+	
+Semuanya atas nama Kiki2323 
+
+*📲 PULSA*
+
+0888 0949 0679
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
