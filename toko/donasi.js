912377ad4cec1a521855d8c3a10120d591ddfd9d
lordrizkyfaisal <96189968+lordrizkyfaisal@users.noreply.github.com>
2021-12-20 15:18:15 +0700

    Update and rename shop/donasi.js to toko/donasi.js

---
+++ toko/donasi.js
@@ -0,0 +1,14 @@
+const donasibot = () => {
+return `DONASI BISA VIA BANK / E-MONEY
+
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ 0882 1337 1423 
+	
+Semuanya atas nama Kiki2323
+`
+	}
+
+exports.donasibot = donasibot
+ 
